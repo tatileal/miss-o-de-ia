7960dd164c67debbe4a8a1dd26ebaeae147ecf89
tatileal <142230415+tatileal@users.noreply.github.com>
2024-09-27 09:21:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,20 @@ function mostraPergunta() {
 }
 mostraPergunta()
 
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
