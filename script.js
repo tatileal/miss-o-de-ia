7ecf401adec08ec7416c1b6907ca80eebdcdbf88
tatileal <142230415+tatileal@users.noreply.github.com>
2024-11-08 09:27:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ const perguntas = [
     {
          enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
-            "Isso é assustador!",
-            "Isso é maravilhoso!"
+             texto: "Isso é assustador!",
+    afirmacao: "No início ficou com medo do que essa tecnologia pode fazer.
+{
+    texto: "Isso é maravilhoso!",
+    afirmacao: "Quis saber como usar IA no seu dia a dia."
+}
         ]
     },
     {
